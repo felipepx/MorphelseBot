da1cbee699d9525b128b4c830bc50988c0bcb217
Felipe Pereira <95257377+felipepx@users.noreply.github.com>
2022-10-07 21:11:44 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-require('dotenv').config();
-const {Client, Intents} = require('discord.js');
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require("./config.json")
 
-const client = new Client({
-    Intents: [
-        Intents.FLAGS.GUILDS,
-        Intents.FLAGS.GUILDS,
-    ],
+
+client.on("ready", () => {
+    console.log(`Bot foi iniciado, com ${client.user.size} usuários, em ${client.chennels.size} canais, em ${client.guilds.size} servidores`);
+    client.user.setGame(`Eu estou em ${client.guilds.size} servidores`);
+
+});
+client.on("guildCreate", guild =>{
+    client.on(`O bot entrou no servidor: ${guild.name} (id: ${guild.id}). População: ${guild.memberCount} membros!`);
+    client.user.setActivity(`Estou em ${client.guilds.size} servidores`);
+})
+
+client.on("guildDelete", guild =>{
+    console.log(`O bot foi removido do servidor: ${guild.name} (id: ${guild.id})`);
+    client.user.setActivity(`Serving ${client.guilds.size} servers`);
 });
 
-client.on('ready', () =>)
+
+client.on("message", async message =>{
+
+});
+
+client.login(config.token);
