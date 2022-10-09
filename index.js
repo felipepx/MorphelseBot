d83e561ef1db3ced34d675ea66116bbea8bf686a
Felipe Pereira <95257377+felipepx@users.noreply.github.com>
2022-10-08 23:01:33 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 const Discord = require("discord.js");
-const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES",] });
 const config = require("./config.json");
 
 
 
 client.on('ready', () => {
-    console.log(`Bot foi iniciado, com ${client.user.size} usuários, em ${client.chennels.size} canais, em ${client.guilds.size} servidores`);
-    client.user.setGame(`Eu estou em ${client.guilds.size} servidores`);
+    console.log(`Bot foi iniciado, com ${client.user.size} usuários, em ${client.channels.size} canais, em ${client.guilds.size} servidores`);
+    client.user.setActivity(`Eu estou em ${client.guilds.size} servidores`);
 
 
 });
+
 
 client.on('guildCreate', guild => {
     client.on(`O bot entrou no servidor: ${guild.name} (id: ${guild.id}). População: ${guild.memberCount} membros!`);
     client.user.setActivity(`Estou em ${client.guilds.size} servidores`);
 
+    
 })
+
 
 client.on("guildDelete", guild => {
     console.log(`O bot foi removido do servidor: ${guild.name} (id: ${guild.id})`);
     client.user.setActivity(`Serving ${client.guilds.size} servers`);
 
+    
 });
 
 
@@ -28,4 +32,8 @@ client.on("message", async message => {
 
 });
 
+
 client.login(config.token);
+
+
+
