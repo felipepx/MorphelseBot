3c678713da3c6e994fcc48aa5520c78180d6651e
Felipe Pereira <95257377+felipepx@users.noreply.github.com>
2022-10-08 23:16:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,25 +6,21 @@ const config = require("./config.json");
 
 client.on('ready', () => {
     console.log(`Bot foi iniciado, com ${client.user.size} usuários, em ${client.channels.size} canais, em ${client.guilds.size} servidores`);
-    client.user.setActivity(`Eu estou em ${client.guilds.size} servidores`);
+    client.user.setActivity(` Morphelses@gmail.com | Lipe&Malbem`);
 
 
 });
-
 
 client.on('guildCreate', guild => {
     client.on(`O bot entrou no servidor: ${guild.name} (id: ${guild.id}). População: ${guild.memberCount} membros!`);
     client.user.setActivity(`Estou em ${client.guilds.size} servidores`);
 
-    
 })
-
 
 client.on("guildDelete", guild => {
     console.log(`O bot foi removido do servidor: ${guild.name} (id: ${guild.id})`);
     client.user.setActivity(`Serving ${client.guilds.size} servers`);
 
-    
 });
 
 
@@ -32,8 +28,4 @@ client.on("message", async message => {
 
 });
 
-
 client.login(config.token);
-
-
-
